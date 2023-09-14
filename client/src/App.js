05e339dd44cc5f1fa7405597f75c9e3c72a2c7e2
Nishanth Kiran <nishanthkiran2@gmail.com>
2023-09-15 00:25:32 +0530

    feat: set up states with redux and routes with react-dom-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import {BrowserRouter as Router, Navigate, Routes, Route} from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
 
 function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+    return (
+        <div className="app">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/profile/:userId" element={<ProfilePage />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
